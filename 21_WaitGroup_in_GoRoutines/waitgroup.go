eff602a97947b339882d7830bfa93063bd57bbd4
rohankarmacharya <rohankarmacharya.biz@gmail.com>
2025-12-12 15:00:39 +0545

    Golang: Basics

---
+++ 21_WaitGroup_in_GoRoutines/waitgroup.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func task(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Doing task", id)
+}
+func main() {
+
+	var wg sync.WaitGroup
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go task(i, &wg)
+	}
+
+	wg.Wait()
+	fmt.Println("All tasks completed")
+}
